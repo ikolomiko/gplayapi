193ca69b5dd490cca494691c08e32566a473692e
İlker Avcı <ikolomiko@gmail.com>
2023-03-28 02:32:22 +0300

    Created DefaultHttpClient to keep compatibility

---
+++ src/main/java/com/aurora/gplayapi/network/DefaultHttpClient.kt
@@ -0,0 +1,19 @@
+/*
+ *     GPlayApi
+ *     Copyright (C) 2023  Aurora OSS
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ */
+
+package com.aurora.gplayapi.network
+
+// This variable can be assigned to FuelClient as well
+var DefaultHttpClient: IHttpClient = OkHttpClient
